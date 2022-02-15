4fa402b977b8e4fc4b6ea8d48a32ad346dc19fa0
sorwnqe <yuqiang>
2022-02-15 23:30:00 +0800

    fix: update tailwind preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,7 @@
+const { defaultPreset } = require('tailwindcss-miniprogram-preset');
+
+/** @type {import('@types/tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
-  mode: 'jit',
-  purge: {
-    content: ['./src/**/*.{vue,js,ts,jsx,tsx,wxml}'],
-  },
-  darkMode: false,
-  separator: '__',
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-  corePlugins: {
-    preflight: false,
-    space: false,
-    divideColor: false,
-    divideOpacity: false,
-    divideStyle: false,
-    divideWidth: false,
-  },
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+  presets: [defaultPreset],
 };
